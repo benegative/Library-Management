376195d4f91bd71fe2ff3eff202c0bcbdba0c349
benegative <khannikkey@gmail.com>
2023-04-07 21:12:55 +0530

    Record details managed

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\django projects\Simple Library Management System in Python Django with Source Code\library-management\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
