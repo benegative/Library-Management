ee6eacc2105bc883476cd3d75889b1830cb7a1ef
benegative <khannikkey@gmail.com>
2023-04-07 22:27:58 +0530

    Finale Summition

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!F:\django projects\Simple Library Management System in Python Django with Source Code\library-management\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
